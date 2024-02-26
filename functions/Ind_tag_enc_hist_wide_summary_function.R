2e586ef6e5ae82c9cd1f856a5604ad0cb65dc4d4
Sam Graf <sfigraf@gmail.com>
2024-02-26 12:31:18 -0700

    in the middle of getting the codes to work with ind function

--- functions/Ind_tag_enc_hist_wide_summary_function.R
+++ functions/Ind_tag_enc_hist_wide_summary_function.R
@@ -2,7 +2,7 @@
 # allDetectionsAndRecaptures <- df_list$Recaps_detections
 # Release <- Release
 # combined_events_stations <- combined_events_stations #resulting df from combined_events and stations function
-# States_summarized <- states_summarized
+# States_summarized <- states_data_list$States_summarized
 #recaps and all detreitons comes from WGFP ENC_hist_function, release data is a read_in csv, all_events_condensed with stations comes from combine_stations_events function
 Ind_tag_enc_hist_wide_summary_function <- function(allDetectionsAndRecaptures, Release, combined_events_stations, States_summarized, markerTags){
   
@@ -12,44 +12,53 @@ Ind_tag_enc_hist_wide_summary_function <- function(allDetectionsAndRecaptures, R
   all_enc12 <- allDetectionsAndRecaptures %>%
     count(TAG, Event, name = "Encounters") 
   
-  all_enc12 <- pivot_wider(data = all_enc12, id_cols = TAG, names_from = Event, values_from = Encounters)
+  all_enc12wide  <- all_enc12 %>%
+    pivot_wider(id_cols = TAG, names_from = Event, values_from = Encounters) %>%
+    rename_with(~ paste0(., "_n"), -TAG) 
   
-  x <- all_enc12 %>%
-    replace_na(list(Species = "No Info", ReleaseSite = "No Info"))
-  
-  ENC_ALL <- all_enc12 %>%
-    rename(RB1_n = RB1,
-           RB2_n = RB2,
-           HP3_n = HP3,
-           HP4_n = HP4,
-           CF5_n = CF5,
-           CF6_n = CF6,
-           CD1_n = CD1,
-           CD2_n = CD2,
-           CS1_n = CS1,
-           CS2_n = CS2,
-           CU1_n = CU1,
-           CU2_n = CU2,
-           M1_n = M1,
-           M2_n = M2,
-           B3_n = B3,
-           B4_n = B4,
-           B5_n = B5,
-           B6_n = B6,
-           Recap_n = Recapture
-    ) %>%
-    select(TAG, RB1_n,RB2_n,HP3_n, HP4_n, CF5_n, CF6_n, CD1_n, CD2_n, CS1_n, CS2_n, CU1_n, CU2_n, M1_n, M2_n, B3_n, B4_n, B5_n, B6_n, Recap_n)
-  
+  #ordering correctly for calculations later on
+  columnOrder <- c(RedBarnCodes, HitchingPostCodes, ConfluenceCodes, ConnectivityChannelDownstreamCodes, ConnectivityChannelSideChannelCodes, ConnectivityChannelUpstreamCodes, 
+                   MobileRunCodes, WindyGapAntennaSiteCode, KaibabParkAntennaSiteCode, RiverRunAntennaSiteCode, FraserRiverCanyonAntennaSiteCode)
+  all_enc12wideOrdered <- all_enc12wide %>%
+    select(TAG, one_of(paste0(columnOrder, "_n")), Recapture_n)
+    #select(TAG, RB1_n,RB2_n,HP3_n, HP4_n, CF5_n, CF6_n, CD1_n, CD2_n, CS1_n, CS2_n, CU1_n, CU2_n, M1_n, M2_n, B3_n, B4_n, B5_n, B6_n, Recap_n)
+    
+  #    
+  # x <- all_enc12 %>%
+  #   replace_na(list(Species = "No Info", ReleaseSite = "No Info"))
+  # 
+  # ENC_ALL <- all_enc12 %>%
+  #   rename(RB1_n = RB1,
+  #          RB2_n = RB2,
+  #          HP3_n = HP3,
+  #          HP4_n = HP4,
+  #          CF5_n = CF5,
+  #          CF6_n = CF6,
+  #          CD1_n = CD1,
+  #          CD2_n = CD2,
+  #          CS1_n = CS1,
+  #          CS2_n = CS2,
+  #          CU1_n = CU1,
+  #          CU2_n = CU2,
+  #          M1_n = M1,
+  #          M2_n = M2,
+  #          B3_n = B3,
+  #          B4_n = B4,
+  #          B5_n = B5,
+  #          B6_n = B6,
+  #          Recap_n = Recapture
+  #   ) %>%
+  #   select(TAG, RB1_n,RB2_n,HP3_n, HP4_n, CF5_n, CF6_n, CD1_n, CD2_n, CS1_n, CS2_n, CU1_n, CU2_n, M1_n, M2_n, B3_n, B4_n, B5_n, B6_n, Recap_n)
+  # 
   
   #### Combine Release data ###
   Release1 <- Release %>%
-    rename(TAG = TagID) %>%
-    mutate(TAG = str_trim(TAG)) %>%
+    mutate(TAG = str_trim(TagID)) %>%
     replace_na(list(Species = "No Info", ReleaseSite = "No Info")) #replaced species and releasesite to follow the same convention as AllEvents
   
   # was geting a massive dataframe because the Release df is called TAGid not TAG.
   #need to actually join on full join not merge
-  ENC_Release <- full_join(Release1, ENC_ALL,  by = "TAG")
+  ENC_Release <- full_join(Release1, all_enc12wideOrdered,  by = "TAG")
   
   #gets tag list that wasn't in release file or markerTags
   unknown_tags <- ENC_Release %>%
@@ -57,32 +66,37 @@ Ind_tag_enc_hist_wide_summary_function <- function(allDetectionsAndRecaptures, R
            !TAG %in% markerTags) %>%
     select(TAG,where(is.numeric))
   
+  ENC_Release[is.na(ENC_Release)] = 0 #gets rest of the number count columns to 0 from NA
   
-  #ENC_Release11$TAG[3433:nrow(ENC_Release11)]
-  ENC_Release[is.na(ENC_Release)]=0 #gets rest of the number count columns to 0 from NA
+  ENC_Release1 <- ENC_Release %>%
+    #applies the mutation logic to all columns that end with "_n". 
+    #It checks if each value is greater than 0 
+    #creates a new column with the name obtained by removing "_n" from the original column name.
+    mutate(across(ends_with("_n"), ~ (. > 0), .names = "{sub('_n', '', .col)}"))
+  
   
   #### Make 1 or 0 for encounter history rather than counts ###
   #gets df with TF of whether a fish was detected at a antenna
-  ENC_Release1 <- ENC_Release %>%
-    mutate(RB1 = (RB1_n >0),
-           RB2 = (RB2_n >0),
-           HP3 = (HP3_n >0),
-           HP4 = (HP4_n >0),
-           CF5 = (CF5_n >0),
-           CF6 = (CF6_n >0),
-           CD1 = (CD1_n >0),
-           CD2 = (CD2_n >0),
-           CS1 = (CS1_n >0),
-           CS2 = (CS2_n >0),
-           CU1 = (CU1_n >0),
-           CU2 = (CU2_n >0),
-           M1 = (M1_n >0),
-           M2 = (M2_n >0),
-           B3 = (B3_n >0),
-           B4 = (B4_n>0),
-           B5 = (B5_n >0),
-           B6 = (B6_n>0),
-           Recapture = (Recap_n > 0))
+  # ENC_Release1 <- ENC_Release %>%
+  #   mutate(RB1 = (RB1_n >0),
+  #          RB2 = (RB2_n >0),
+  #          HP3 = (HP3_n >0),
+  #          HP4 = (HP4_n >0),
+  #          CF5 = (CF5_n >0),
+  #          CF6 = (CF6_n >0),
+  #          CD1 = (CD1_n >0),
+  #          CD2 = (CD2_n >0),
+  #          CS1 = (CS1_n >0),
+  #          CS2 = (CS2_n >0),
+  #          CU1 = (CU1_n >0),
+  #          CU2 = (CU2_n >0),
+  #          M1 = (M1_n >0),
+  #          M2 = (M2_n >0),
+  #          B3 = (B3_n >0),
+  #          B4 = (B4_n>0),
+  #          B5 = (B5_n >0),
+  #          B6 = (B6_n>0),
+  #          Recapture = (Recap_n > 0))
   
   
   #summary stats of each antenna encounter
@@ -90,33 +104,50 @@ Ind_tag_enc_hist_wide_summary_function <- function(allDetectionsAndRecaptures, R
   #but also has release data
   totalcols <- ncol(ENC_Release1)
   
-  ENC_Release2 <- ENC_Release1 %>%
+  ENC_Release22 <- ENC_Release1 %>%
     #counts number of TRUE across specified rows. -SG
     # need to have parentheses (totalcols-1) that's why i was getting bad numbers on biomark T/F initially
     #added 8 new columns for new antennas
     mutate(
-      TotalEncounters = rowSums(ENC_Release1[(totalcols-18):totalcols] == TRUE),
-      TotalAntennas1 = rowSums(ENC_Release1[(totalcols-18):(totalcols-1)] == TRUE),
-      TotalStationary = rowSums(ENC_Release1[(totalcols-18):(totalcols-7)] == TRUE),
-      TotalMobile = rowSums(ENC_Release1[(totalcols-6):(totalcols-5)] == TRUE),
-      TotalBiomark = rowSums(ENC_Release1[(totalcols-4):(totalcols-1)] == TRUE),
-      TotalRB = rowSums(ENC_Release1[(totalcols-18):(totalcols-17)] == TRUE),
-      TotalHP = rowSums(ENC_Release1[(totalcols-16):(totalcols-15)] == TRUE),
-      TotalCF = rowSums(ENC_Release1[(totalcols-14):(totalcols-13)] == TRUE),
-      TotalCD = rowSums(ENC_Release1[(totalcols-12):(totalcols-9)] == TRUE),
-      TotalCU = rowSums(ENC_Release1[(totalcols-8):(totalcols-7)] == TRUE),
+      TotalEncounters = rowSums(select(., dplyr::ends_with(c(RedBarnCodes, HitchingPostCodes, ConfluenceCodes, 
+                                            ConnectivityChannelDownstreamCodes, ConnectivityChannelSideChannelCodes,
+                                            ConnectivityChannelUpstreamCodes, MobileRunCodes, 
+                                            WindyGapAntennaSiteCode, KaibabParkAntennaSiteCode,
+                                            RiverRunAntennaSiteCode, FraserRiverCanyonAntennaSiteCode, "Recapture"))) == TRUE),
+      TotalAntennas1 = rowSums(select(., dplyr::ends_with(c(RedBarnCodes, HitchingPostCodes, ConfluenceCodes, 
+                                                            ConnectivityChannelDownstreamCodes, ConnectivityChannelSideChannelCodes,
+                                                            ConnectivityChannelUpstreamCodes, MobileRunCodes, 
+                                                            WindyGapAntennaSiteCode, KaibabParkAntennaSiteCode,
+                                                            RiverRunAntennaSiteCode, FraserRiverCanyonAntennaSiteCode))) == TRUE),
+      TotalStationary = rowSums(select(., dplyr::ends_with(c(RedBarnCodes, HitchingPostCodes, ConfluenceCodes, 
+                                                             ConnectivityChannelDownstreamCodes, ConnectivityChannelSideChannelCodes,
+                                                             ConnectivityChannelUpstreamCodes, MobileRunCodes, 
+                                                             WindyGapAntennaSiteCode, KaibabParkAntennaSiteCode,
+                                                             RiverRunAntennaSiteCode, FraserRiverCanyonAntennaSiteCode))) == TRUE),
+
+      TotalMobile = rowSums(select(.,  dplyr::matches(paste0("(", MobileRunCodes, ")_n"))) == TRUE),
+      TotalBiomark = rowSums(select(., dplyr::matches(paste0("(", c(WindyGapAntennaSiteCode, KaibabParkAntennaSiteCode,
+                                                                                     RiverRunAntennaSiteCode, FraserRiverCanyonAntennaSiteCode), ")_n"))) == TRUE),
+      TotalRedBarn = rowSums(select(., dplyr::matches(paste0("(", RedBarnCodes, ")_n"))) == TRUE),
+      TotalHitchingPost = rowSums(select(., dplyr::matches(paste0("(", HitchingPostCodes, ")_n"))) == TRUE),
+      TotalConfluence = rowSums(select(., dplyr::matches(paste0("(", ConfluenceCodes, ")_n"))) == TRUE),
+      TotalConnectivityDownstream = rowSums(select(., dplyr::matches(paste0("(", ConnectivityChannelDownstreamCodes, ")_n"))) == TRUE),
+      TotalConnectivitySideChannel = rowSums(select(., dplyr::matches(paste0("(", ConnectivityChannelSideChannelCodes, ")_n"))) == TRUE),
+      TotalConnectivityUpstream = rowSums(select(., dplyr::matches(paste0("(", ConnectivityChannelUpstreamCodes, ")_n"))) == TRUE),
     ) %>%
     # just says if the fish was ever detected at these sites
-    mutate(RB = (RB1_n > 0 | RB2_n >0),
-           HP = (HP3_n > 0 | HP4_n >0),
-           CF = (CF5_n > 0 | CF6_n >0),
-           CD = (CD1_n > 0 | CD2_n >0),
-           CS = (CS1_n > 0 | CS2_n >0),
-           CU = (CU1_n > 0 | CU2_n >0),
-           Biomark = (B3_n > 0 | B4_n >0 | B5_n > 0 | B6_n >0),
-           Mobile = (M1_n > 0 | M2_n >0)) %>%
-    filter(!UTM_X %in% c(0, NA)) # one way to filter out tags that don't have any sort of release data; usually if they're entered in release file then they have UTM's
+    mutate(RedBarn = TotalRedBarn > 0,
+           HitchingPost = TotalHitchingPost > 0,
+           Confluence = TotalConfluence > 0,
+           ConnectivityDownstream = TotalConnectivityDownstream > 0,
+           ConnectivitySideChannel = TotalConnectivitySideChannel > 0,
+           ConnectivityUpstream = TotalConnectivityUpstream > 0,
+           Biomark = TotalBiomark > 0,
+           Mobile = TotalMobile >0
+    ) %>%
+    filter(TAG %in% unique(Release1$TAG)) 
   
+  setdiff(ENC_Release22$TotalHitchingPost, ENC_Release2$TotalHitchingPost)
   ###Bringing in Station data with info about ABOVE/BELOW dam for joining
   ### the release data isn't being brought in well; The tags aren't being brought in as full numbers, so when the release data is joined,
   # it can't match up 23000088888 to 2.3E+11; so release site gets put in as "no info", and
