e65a844f9272d9bd66bdfe9189ec3f789c5d95f8
sfigraf <sfigraf@gmail.com>
2024-02-28 15:01:33 -0700

    making sure test tags are filtered correctly

--- functions/addUTMsAndReformatStationary.R
+++ functions/addUTMsAndReformatStationary.R
@@ -2,20 +2,21 @@
 
 ##not sure if we want to return marker tags too
 
-cleanStationary <- function(Stationary){
+addUTMsAndReformatStationary <- function(Stationary){
   start_time <- Sys.time()
   print("Reformatting and adding UTMs to Stationary Detection File......")
   
   Stationary_withUTMS <- Stationary %>%
+    #taking out test_tags
+    #this variable comes from the metadata
+    filter(!TAG %in% test_tags) %>%
     mutate(TAG = ifelse(str_detect(TAG, "^900"), str_sub(TAG, 4,-1), TAG)) %>%
     # mutate(TAG = gsub("\\_", "", str_trim(TAG)), 
     #        DTY = ifelse(str_detect(DTY, "/"),
     #                 as.character(mdy(DTY)),
     #                 DTY)
     #        ) %>%
-    #taking out test_tags
-    #this variable comes from the metadata
-    filter(!TAG %in% test_tags) %>%
+    
     #the frontend/backend part should be unnecessary since the backend and frontend site code names should be the same with stationary antennas but it's good to be consistent
     #the UTMs are v necessary
     left_join(wgfpMetadata$AntennaMetadata[,c("FrontendSiteCode", "BackendSiteCode", "UTM_X", "UTM_Y")], by = c("SCD" = "BackendSiteCode")) %>%
@@ -33,7 +34,7 @@ cleanStationary <- function(Stationary){
   #   left_join(selectedMetaData, by = c("SCD" = "FrontendSiteCode")) %>%
     
   end_time = Sys.time()
-  print(paste("Cleaning Stationary file took", round((end_time-start_time),2)))
+  print(paste("Adding UTMs and reformatting Statoinary Detection file took", round((end_time-start_time),2), "seconds."))
   
   return(Stationary_withUTMS)
 }
