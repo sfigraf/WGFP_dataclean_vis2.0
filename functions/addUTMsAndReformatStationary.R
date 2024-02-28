b62e169293115dc297ee0de53f9cd4fc6e5ccf47
sfigraf <sfigraf@gmail.com>
2024-02-28 15:15:29 -0700

    clean up

--- functions/addUTMsAndReformatStationary.R
+++ functions/addUTMsAndReformatStationary.R
@@ -1,6 +1,5 @@
-#### stationary data cleaning runscript
-
-##not sure if we want to return marker tags too
+#### stationary data utm assigning function
+#marker tags are included in the return, but test tags are filtered out
 
 addUTMsAndReformatStationary <- function(Stationary){
   start_time <- Sys.time()
@@ -11,27 +10,12 @@ addUTMsAndReformatStationary <- function(Stationary){
     #this variable comes from the metadata
     filter(!TAG %in% test_tags) %>%
     mutate(TAG = ifelse(str_detect(TAG, "^900"), str_sub(TAG, 4,-1), TAG)) %>%
-    # mutate(TAG = gsub("\\_", "", str_trim(TAG)), 
-    #        DTY = ifelse(str_detect(DTY, "/"),
-    #                 as.character(mdy(DTY)),
-    #                 DTY)
-    #        ) %>%
-    
     #the frontend/backend part should be unnecessary since the backend and frontend site code names should be the same with stationary antennas but it's good to be consistent
-    #the UTMs are v necessary
+    ## getting UTM's are important because they are plotted later when getting stations file in GIS
     left_join(wgfpMetadata$AntennaMetadata[,c("FrontendSiteCode", "BackendSiteCode", "UTM_X", "UTM_Y")], by = c("SCD" = "BackendSiteCode")) %>%
     mutate(SCD = coalesce(FrontendSiteCode, SCD)) %>%
     select(-FrontendSiteCode) %>%
     distinct() 
-  
-
-  # takes out 900 from TAG in WGFP Clean
-  # also takes out duplicate rows
-  # Stationary_withUTMS <- Stationary_cleanedTime1 %>%
-  #   #this change
-  #   
-  #   # assigning UTM's are important because they are plotted later when getting stations file in GIS
-  #   left_join(selectedMetaData, by = c("SCD" = "FrontendSiteCode")) %>%
     
   end_time = Sys.time()
   print(paste("Adding UTMs and reformatting Statoinary Detection file took", round((end_time-start_time),2), "seconds."))
