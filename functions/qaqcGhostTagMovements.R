b21902aa3d80da085a710cf8be57becda2599f84
sfigraf <sfigraf@gmail.com>
2024-03-12 14:37:23 -0600

    added notes into the qaqc check

--- functions/qaqcGhostTagMovements.R
+++ functions/qaqcGhostTagMovements.R
@@ -1,7 +1,7 @@
 qaqcGhostTagMovements <- function(GhostTags, Movements_df){
   
   ghosttagsCondensed <- GhostTags %>%
-    select(TagID, GhostDate) %>%
+    select(TagID, GhostDate, Notes) %>%
     left_join(Movements_df, by = c("TagID" = "TAG"))
   
   ghostTagsWithMovementAfterGhostDate <- ghosttagsCondensed %>%
@@ -11,7 +11,8 @@ qaqcGhostTagMovements <- function(GhostTags, Movements_df){
       GhostDate = unique(GhostDate),
       antennasDetectedAfterGhostDate = paste(unique(det_type), collapse = ", "),
       total_distmovedAfterGhostDate = (sum(abs(dist_moved), na.rm = TRUE)),
-      maxUpstreamDistMovedAfterGhost = max(dist_moved)) %>%
+      maxUpstreamDistMovedAfterGhost = max(dist_moved), 
+      Notes = unique(Notes)) %>%
     filter(total_distmovedAfterGhostDate > 0) %>%
     arrange(desc(total_distmovedAfterGhostDate))
   
