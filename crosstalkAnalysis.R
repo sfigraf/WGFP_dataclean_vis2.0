068341b521c8849f4db599080e51363c4089dded
Sam Graf <sfigraf@gmail.com>
2024-03-13 17:21:47 -0600

    about to start making this a functoin

--- crosstalkAnalysis.R
+++ crosstalkAnalysis.R
@@ -1,22 +1,28 @@
 ####crosstalk analysis to see duplicate detections/exact same timestamp
 #want to add this qaqc tab
 
-allDetections <- combinedData_df_list$All_Events
-cd1cd2 <- allDetections %>%
-  filter(Event %in% c("CD1", "CD2"), 
-         !str_detect(TAG, "^0000000|^999")) 
+
+
+metaDataVariableNames$ConnectivityChannelDownstreamFrontendCodes
+
+#allEvents doesn't have marker tags in it
+All_Events <- combinedData_df_list$All_Events
+
+cd1cd2 <- All_Events %>%
+  filter(Event %in% metaDataVariableNames$ConnectivityChannelDownstreamFrontendCodes) 
 
 cd1cd2Crosstalk <- cd1cd2 %>%
   group_by(Datetime, TAG) %>%
   summarise(antennas = paste(unique(Event), collapse = ", ")) %>%
   filter(nchar(antennas) > 3)
 
+(nrow(cd1cd2Crosstalk)/nrow(cd1cd2))*100
+
 (30/3280)*100
 
 ########rb2=1, 2
-rb1rb2 <- allDetections %>%
-  filter(Event %in% c("RB1", "RB2"), 
-         !str_detect(TAG, "^0000000|^999")) 
+rb1rb2 <- All_Events %>%
+  filter(Event %in% metaDataVariableNames$RedBarnFrontendCodes) 
 
 rb1rb2Crosstalk <- rb1rb2 %>%
   group_by(Datetime, TAG) %>%
@@ -26,7 +32,7 @@ rb1rb2Crosstalk <- rb1rb2 %>%
 (81/168327)*100
 
 #######
-hp1hp2 <- allDetections %>%
+hp1hp2 <- All_Events %>%
   filter(Event %in% c("HP3", "HP4"), 
          !str_detect(TAG, "^0000000|^999")) 
 
@@ -38,7 +44,7 @@ hp1hp2Crosstalk <- hp1hp2 %>%
 (72/1105413)*100
 
 ##
-CF1CF2 <- allDetections %>%
+CF1CF2 <- All_Events %>%
   filter(Event %in% c("CF5", "CF6"), 
          !str_detect(TAG, "^0000000|^999")) 
 
@@ -50,7 +56,7 @@ CF1CF2Crosstalk <- CF1CF2 %>%
 (1/417614)*100
 
 ##CS Crosstalk
-CS1CS2 <- allDetections %>%
+CS1CS2 <- All_Events %>%
   filter(Event %in% c("CS1", "CS2"), 
          !str_detect(TAG, "^0000000|^999")) 
 
