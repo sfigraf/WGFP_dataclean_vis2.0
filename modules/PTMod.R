cb10a41800b2fc451d55807923bb657ff890cfc7
Sam Graf <sfigraf@gmail.com>
2024-04-05 18:54:22 -0600

    added date slider, looking good

--- modules/PTMod.R
+++ modules/PTMod.R
@@ -16,19 +16,30 @@ PT_UI <- function(id, PTData) {
                                label = "Variable to Plot",
                                choices = colnames(PTData)[grepl("_", colnames(PTData))],
                                selected = colnames(PTData)[grepl("_", colnames(PTData))][1],
-                               )
+                               ), 
+                   sliderInput(ns("dateSlider"), "Date",
+                               min = min(lubridate::date(PTData$DateTime) -1),
+                               max = max(lubridate::date(PTData$DateTime) +1),  
+                               value = c(min(lubridate::date(PTData$DateTime) -1), max(lubridate::date(PTData$DateTime) +1)),
+                               step = 1,
+                               timeFormat = "%d %b %y",
+                               #animate = animationOptions(interval = 500, loop = FALSE)
+                   ),
                    #want to make a date range input here
 
       ),
-      mainPanel(
+      mainPanel(width = 10,
         tabsetPanel(
           #tabPanel("PT Data",
         #                      br(),
         #                      
         # ),
         tabPanel("PT Data Plot",
-                 br(),
-                 withSpinner(plotlyOutput(ns("PTPlot"))),
+                 box(
+                   width = 10,
+                   withSpinner(plotlyOutput(ns("PTPlot")))
+                 )
+                 
                  #withSpinner(DT::dataTableOutput(ns("allevents1"))),
         )
         )#end of tabset panel
@@ -44,22 +55,26 @@ PT_Server <- function(id, PTData) {
     function(input, output, session) {
       
       filteredPTData <- reactive({
+        req(input$sitePicker)
         filteredPTData <- PTData %>%
           select(Site, DateTime, input$variableSelect) %>%
-          dplyr::filter(Site %in% input$sitePicker)
+          dplyr::filter(Site %in% input$sitePicker, 
+                        lubridate::date(DateTime) >= input$dateSlider[1] & lubridate::date(DateTime) <= input$dateSlider[2])
         return(filteredPTData)
       })
       
       output$PTPlot <- renderPlotly({
-        filteredPTData() %>%
-          ggplot(aes_string(x = "DateTime", y = input$variableSelect, color = "Site"
-                     #text = paste('Date: ', as.character(Date), '\n')
-                     )
-          ) +
-          geom_line() +
-          theme_classic() +
-          labs(title="Pressure Transducer Data",
-               x = "Date", y = input$variableSelect)
+        req(input$sitePicker)
+          filteredPTData() %>%
+            ggplot(aes_string(x = "DateTime", y = input$variableSelect, color = "Site"
+            )
+            ) +
+            geom_line() +
+            theme_classic() +
+            labs(title="Pressure Transducer Data",
+                 x = "Date", y = input$variableSelect)
+        
+        
       })
 
     }
