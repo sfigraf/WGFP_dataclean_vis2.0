2aa7dc219afd04531cd9c0135b593d6b8e44ddea
Sam Graf <sfigraf@gmail.com>
2024-05-02 12:56:14 -0600

    code of how I changed sites for Biomark

--- miscR/map_polygon_readins.R
+++ miscR/map_polygon_readins.R
@@ -11,6 +11,25 @@ latLongCRS <- st_crs("+proj=longlat +datum=WGS84 +no_defs") #should be same as +
 
 antenna_sites <- st_transform(read_sf(file.path(layerLocation, "antenna_sites1.shp")), latLongCRS)
 
+antenna_sites$SiteName <- gsub("Kaibab Park", "Granby Diversion", antenna_sites$SiteName)
+antenna_sites$SiteName <- gsub("Below Windy Gap Dam", "Windy Gap Bypass Channel", antenna_sites$SiteName)
+antenna_sites$SiteLabel <- gsub("B3", "WG1", antenna_sites$SiteLabel)
+antenna_sites$SiteLabel <- gsub("B4", "GD1", antenna_sites$SiteLabel)
+antenna_sites$SiteLabel <- gsub("B5", "RR1", antenna_sites$SiteLabel)
+antenna_sites$SiteLabel <- gsub("B6", "FC1", antenna_sites$SiteLabel)
+
+st_write(antenna_sites, "antenna_sites2.shp")
+leaflet() %>%
+  addTiles() %>%
+  addAwesomeMarkers(data = antenna_sites,
+                    icon = Station_icons,
+                    clusterOptions = markerClusterOptions(),
+                    label = paste(antenna_sites$SiteLabel),
+                    popup = paste(antenna_sites$SiteName, "<br>",
+                                  "Channel Width:", antenna_sites$ChannelWid, "feet"),
+                    group = "Antennas")
+
+
 stream_centerline <- st_transform(read_sf(file.path(layerLocation, "Stream_Centerline_Post.shp")), latLongCRS)
 
 releasesites <- st_transform(read_sf(file.path(layerLocation, "ReleaseSites2021.shp")), latLongCRS)
