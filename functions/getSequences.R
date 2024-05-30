fc43654434fdfe7516461847c0c6f09b6f7ce704
Sam Graf <sfigraf@gmail.com>
2024-05-30 13:51:07 -0600

    gets upstream and downstream instances of selected antennas

--- functions/getSequences.R
+++ functions/getSequences.R
@@ -12,10 +12,13 @@ compute_time_diff <- function(start, end) {
 }
 
 extract_sequences <- function(tag_data, downstreamAntennas, upstreamAntennas) {
-  sequences <- data.frame(TAG = character(), DatetimeDetectedAtDownstreamAntennas = as.POSIXct(character()), DatetimeDetectedAtUpstreamAntennas = as.POSIXct(character()), stringsAsFactors = FALSE)
+  sequences <- data.frame(TAG = character(), 
+                          DatetimeDetectedAtDownstreamAntennas = as.POSIXct(character()), 
+                          DatetimeDetectedAtUpstreamAntennas = as.POSIXct(character()), 
+                          MovementDirection = character(), 
+                          stringsAsFactors = FALSE)
   i <- 1
   
-  # Initialize a while loop to iterate through the tag_data dataframe
   while (i < nrow(tag_data)) {
     
     # Find indices of events that match the chosen downstream antennas within the subset of tag_data starting from the current index i
@@ -30,24 +33,55 @@ extract_sequences <- function(tag_data, downstreamAntennas, upstreamAntennas) {
       # Get the absolute position in tag_data of the first upstream antenna event found
       upstreamAntennas_first <- i + upstreamAntennas_index[1] - 1
       
-      # Find the last downstream antenna event that occurs before the first upstream antenna event
-      downstreamAntennas_before <- downstreamAntennas_index[downstreamAntennas_index < (upstreamAntennas_first - i + 1)]
+      # Get the absolute position in tag_data of the first downstream antenna event found
+      downstreamAntennas_first <- i + downstreamAntennas_index[1] - 1
       
-      if (length(downstreamAntennas_before) > 0) {
-        downstreamAntennas_last <- i + downstreamAntennas_before[length(downstreamAntennas_before)] - 1
+      # Movement from Downstream to Upstream
+      if (tag_data$Datetime[upstreamAntennas_first] > tag_data$Datetime[downstreamAntennas_first]) {
+        # Find the last downstream antenna event that occurs before the first upstream antenna event
+        downstreamAntennas_before <- downstreamAntennas_index[downstreamAntennas_index < (upstreamAntennas_first - i + 1)]
         
-        # Append a new row to the sequences dataframe with TAG, datetime of last downstream detection, and datetime of first upstream detection
-        sequences <- rbind(sequences, data.frame(
-          TAG = tag_data$TAG[upstreamAntennas_first], 
-          DatetimeDetectedAtDownstreamAntennas = tag_data$Datetime[downstreamAntennas_last], 
-          DatetimeDetectedAtUpstreamAntennas = tag_data$Datetime[upstreamAntennas_first]
-        ))
+        if (length(downstreamAntennas_before) > 0) {
+          downstreamAntennas_last <- i + downstreamAntennas_before[length(downstreamAntennas_before)] - 1
+          
+          # Append a new row to the sequences dataframe
+          sequences <- rbind(sequences, data.frame(
+            TAG = tag_data$TAG[upstreamAntennas_first], 
+            DatetimeDetectedAtDownstreamAntennas = tag_data$Datetime[downstreamAntennas_last], 
+            DatetimeDetectedAtUpstreamAntennas = tag_data$Datetime[upstreamAntennas_first],
+            MovementDirection = "Upstream"
+          ))
+          
+          # Update the index to the position after the first upstream antenna event found
+          i <- upstreamAntennas_first + 1
+        } else {
+          # If no downstream event occurs before the upstream event, skip to the next upstream event
+          i <- upstreamAntennas_first + 1
+        }
+      }
+      
+      # Movement from Upstream to Downstream
+      if (tag_data$Datetime[downstreamAntennas_first] > tag_data$Datetime[upstreamAntennas_first]) {
+        # Find the last upstream antenna event that occurs before the first downstream antenna event
+        upstreamAntennas_before <- upstreamAntennas_index[upstreamAntennas_index < (downstreamAntennas_first - i + 1)]
         
-        # Update the index to the position after the first upstream antenna event found
-        i <- upstreamAntennas_first + 1
-      } else {
-        # If no downstream event occurs before the upstream event, skip to the next upstream event
-        i <- upstreamAntennas_first + 1
+        if (length(upstreamAntennas_before) > 0) {
+          upstreamAntennas_last <- i + upstreamAntennas_before[length(upstreamAntennas_before)] - 1
+          
+          # Append a new row to the sequences dataframe
+          sequences <- rbind(sequences, data.frame(
+            TAG = tag_data$TAG[downstreamAntennas_first], 
+            DatetimeDetectedAtDownstreamAntennas = tag_data$Datetime[downstreamAntennas_first], 
+            DatetimeDetectedAtUpstreamAntennas = tag_data$Datetime[upstreamAntennas_last],
+            MovementDirection = "Downstream"
+          ))
+          
+          # Update the index to the position after the first downstream antenna event found
+          i <- downstreamAntennas_first + 1
+        } else {
+          # If no upstream event occurs before the downstream event, skip to the next downstream event
+          i <- downstreamAntennas_first + 1
+        }
       }
     } else {
       # Exit the loop if there are no more upstream or downstream antenna events in the remaining data
@@ -58,7 +92,6 @@ extract_sequences <- function(tag_data, downstreamAntennas, upstreamAntennas) {
   return(sequences)
 }
 
-## sequences function
 summarizedDf <- function(All_Events, downstreamAntennas, upstreamAntennas){
   df_filtered <- All_Events %>%
     filter(grepl(paste0("^(", paste(c(downstreamAntennas, upstreamAntennas), collapse = "|"), ")"), Event)) %>%
@@ -69,15 +102,11 @@ summarizedDf <- function(All_Events, downstreamAntennas, upstreamAntennas){
     group_by(TAG) %>%
     do(extract_sequences(., downstreamAntennas, upstreamAntennas))
   
-  # Add columns for movement direction and time difference
+  # Add columns for time difference
   newDF2 <- newDF %>%
-    mutate(`Upstream Or Downstream Movement` = case_when(
-      DatetimeDetectedAtDownstreamAntennas > DatetimeDetectedAtUpstreamAntennas ~ "Downstream", 
-      DatetimeDetectedAtDownstreamAntennas < DatetimeDetectedAtUpstreamAntennas ~ "Upstream"
-    ), 
-    `Time Between US/DS Detections (User Friendly)` = mapply(compute_time_diff, DatetimeDetectedAtUpstreamAntennas, DatetimeDetectedAtDownstreamAntennas),
-    `Time Between US/DS Detections (For Sorting)` = difftime(DatetimeDetectedAtDownstreamAntennas, DatetimeDetectedAtUpstreamAntennas, units = "secs")
-    )
+    mutate(`Time Between US/DS Detections (User Friendly)` = mapply(compute_time_diff, DatetimeDetectedAtUpstreamAntennas, DatetimeDetectedAtDownstreamAntennas),
+           `Time Between US/DS Detections (For Sorting)` = difftime(DatetimeDetectedAtDownstreamAntennas, DatetimeDetectedAtUpstreamAntennas, units = "secs"))
+  
   return(newDF2)
 }
 
