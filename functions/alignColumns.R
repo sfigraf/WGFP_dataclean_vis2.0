cc4125cc2193c475dd3c54c98d55e48a2ec92650
sfigraf <sfigraf@gmail.com>
2024-11-14 12:34:47 -0700

    ran data with new alogjn columnsfunction

--- functions/alignColumns.R
+++ functions/alignColumns.R
@@ -1,13 +1,13 @@
 #helper function to easily convert columns of different Dfs to the same columns in order to bind them
-#currently doesn't change the types of data though if 2 dfs had the same column name but different types
+#converts columns with same names to the same type (will break if formats are incorrect, ie Dates, times)
+#then creates new columns with the correct types based off dfWIthIdealColumns
 alignColumns <- function(dfToChange, desiredColumns, dfWithIdealColumns) {
   
   commonColumns <- intersect(colnames(dfToChange), colnames(dfWithIdealColumns))
   
   for (col in commonColumns) {
     expectedType <- class(dfWithIdealColumns[[col]])
-    #print(paste(col, expectedType))
-    # if(matches(class(dfToChange[[col]])) != matches(expectedType)){
+
     if(!setequal(class(dfToChange[[col]]), expectedType)){
       dfToChange[[col]] <- switch(expectedType,
                                   "integer" = as.integer(dfToChange[[col]]),
@@ -17,18 +17,12 @@ alignColumns <- function(dfToChange, desiredColumns, dfWithIdealColumns) {
                                   "Date" = as.Date(dfToChange[[col]]),
                                   dfToChange[[col]])  # Retain original if type doesn't match known types
     }
-   # print(paste(col, expectedType))
     
   }
   
   
   # Create missing columns in dfToChange with NA values
   missingColumns <- setdiff(desiredColumns, colnames(dfToChange))
-  #x1 <- janitor::compare_df_cols(dfToChange, dfWithIdealColumns, return = "mismatch")
-  # common <- names(dfToChange)[names(dfToChange) %in% names(dfWithIdealColumns)]
-  # dfToChange[common] <- lapply(common, function(x) {
-  #   match.fun(paste0("as.", class(dfWithIdealColumns[[x]])[1]))(dfToChange[[x]])
-  # })
   
   if (length(missingColumns) > 0) {
     for (col in missingColumns) {
