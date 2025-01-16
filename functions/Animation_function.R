3abb245b52d290448c7839e454ad79f4cae84fe8
sfigraf <sfigraf@gmail.com>
2025-01-16 12:09:50 -0700

    moving works in app

--- functions/Animation_function.R
+++ functions/Animation_function.R
@@ -15,7 +15,9 @@ Animation_function <- function(Movements_df){
       #makes sense to use floor not cieling with weeks because then there are are more fish in week 0
       # if you want to start at week 1 instead of week 0, add +1 to the end of expression
       # when you change this too, it changes the number of entries in the states dataframe
-      weeks_since = as.numeric(floor(difftime(Date, min(Date), units = "weeks")))
+      weeks_since = as.numeric(floor(difftime(Date, min(Date), units = "weeks"))), 
+      hours_since = as.numeric(floor(difftime(Date, min(Date), units = "hours"))), 
+      
       #months_since = as.numeric(floor(difftime(Date, min(Date), units = "months")))
     ) %>%
     ungroup()
@@ -42,6 +44,7 @@ Animation_function <- function(Movements_df){
     
   
   num_weeks <- max(m1$weeks_since) - min(m1$weeks_since) + 1
+  num_hours <- max(m1$hours_since) - min(m1$hours_since) + 1
   num_days <- max(m1$days_since) - min(m1$days_since) + 1
   list1 <- list("num_weeks" = num_weeks, "num_days" = num_days, "data" = w1, "coords1" = coords1)
   return(list1)
@@ -49,6 +52,9 @@ Animation_function <- function(Movements_df){
 
 # basemaps::set_defaults(map_service = "esri", map_type = "world_imagery")
 # 
+
+# w2 <- w1 %>%
+#   filter(Date > as.Date("2021-04-02"))
 # map_with_data <- ggplot() +
 #   basemap_gglayer(coords1) +
 #   scale_fill_identity() +
@@ -56,17 +62,17 @@ Animation_function <- function(Movements_df){
 #   theme_classic() +
 #   guides(size = 'none', color = guide_legend(title = "Movement"))
 # 
-# map_with_data <- map_with_data + 
-#   geom_point(data = w1, aes(x = w1$X.1, y = w1$Y.1,
+# map_with_data <- map_with_data +
+#   geom_point(data = w1, aes(x =X.1, y = Y.1,
 #                                                   size = 10,
-#                                                   color = w1$movement_only, group = w1$days_since))+
-#   transition_time(days_since) + 
+#                                                   color = movement_only, group = weeks_since))+
+#   transition_time(weeks_since) +
 #   labs(title = "Days") +
 #   ggtitle(
-#     
+# 
 #     paste('{frame_time}'),
 #     subtitle = paste("Day {frame} of {nframes} past Initial Date of", min(w1$Date) ))
-# animate(map_with_data, nframes = num_days,   height = 1200, width =1200)
+# animate(map_with_data, nframes = num_weeks,   height = 1200, width =1200)
 # crs(spdf)
 # 
 # bounds <- st_as_sfc(st_bbox(spdf))
@@ -85,36 +91,36 @@ Animation_function <- function(Movements_df){
 #spdf1 <-st_transform(st_as_sfc(spdf), crs = 3857)
 # 
 #############WORKS FOR slow moving tranisitons 
-x <- ggplot() +
-  basemap_gglayer(coords1) +
-  scale_fill_identity() +
-  coord_sf() +
-  theme_classic() +
-  #####CHANGING COLOR changes the transition "movement" of the fish
-  geom_sf(data = w1, aes(
-                         size = 10, color = movement_only, group = TAG)) +
-  transition_time(weeks_since) +
-  #transition_states(TAG) +
-  coord_sf() +
-  #transition_time(weeks_since)  +
-  ggtitle(
-    #paste("Date", m3$Date),
-    paste("test ", '{frame_time}'),
-    subtitle = paste("Week {frame} of {nframes} past Initial Date of", min(spdf$Date) ))
-  # enter_fade() +
-  # exit_fade()
-  # basemap_gglayer(list1$coords1) +
-  # coord_sf(default_crs = sf::st_crs(4326))
-gganimate::animate(x, nframes = num_weeks, end_pause = 5)
-
-###################
-iris$group <- seq_len(nrow(iris))
-anim1 <- ggplot(iris, aes(Sepal.Width, Petal.Width, group = group)) +
-  geom_point() +
-  labs(title = "{closest_state}") +
-  transition_states(Species, transition_length = 3, state_length = 1) +
-  enter_fade() +
-  exit_fade()
+# x <- ggplot() +
+#   basemap_gglayer(coords1) +
+#   scale_fill_identity() +
+#   coord_sf() +
+#   theme_classic() +
+#   #####CHANGING COLOR changes the transition "movement" of the fish
+#   geom_sf(data = w1, aes(
+#                          size = 10, color = movement_only, group = TAG)) +
+#   transition_time(weeks_since) +
+#   #transition_states(TAG) +
+#   coord_sf() +
+#   #transition_time(weeks_since)  +
+#   ggtitle(
+#     #paste("Date", m3$Date),
+#     paste("test ", '{frame_time}'),
+#     subtitle = paste("Week {frame} of {nframes} past Initial Date of", min(spdf$Date) ))
+#   # enter_fade() +
+#   # exit_fade()
+#   # basemap_gglayer(list1$coords1) +
+#   # coord_sf(default_crs = sf::st_crs(4326))
+# gganimate::animate(x, nframes = num_weeks, end_pause = 5)
+# 
+# ###################
+# iris$group <- seq_len(nrow(iris))
+# anim1 <- ggplot(iris, aes(Sepal.Width, Petal.Width, group = group)) +
+#   geom_point() +
+#   labs(title = "{closest_state}") +
+#   transition_states(Species, transition_length = 3, state_length = 1) +
+#   enter_fade() +
+#   exit_fade()
 
 #animate(x, nframes = num_days,  renderer = gifski_renderer(), height = 1200, width =1200)
 # x
--- modules/animationMod.R
+++ modules/animationMod.R
@@ -13,7 +13,7 @@ mod_animationUI <- function(id) {
              sliderInput(ns("fps_Slider"), "Select frames per Second",
                          min = 0,
                          max = 15,
-                         value = 2,
+                         value = 10,
                          step = .2),
       ),#end of column
       column(width = 4, 
@@ -45,7 +45,7 @@ mod_animationServer <- function(id, filtered_movements_data) {
           scale_fill_identity() +
           coord_sf() +
           theme_classic() +
-          guides(size = FALSE, color = guide_legend(title = "Movement"))
+          guides(size = 'none', color = guide_legend(title = "Movement"))
         
         output$plot12 <- renderImage(
           {
@@ -55,31 +55,35 @@ mod_animationServer <- function(id, filtered_movements_data) {
             isolate(
               if (input$radio2 == "weeks"){
                 map_with_data <- map_with_data + 
-                  geom_point(data = animationDatalist()$data, aes(x = animationDatalist()$data$X.1, y = animationDatalist()$data$Y.1,
+                  geom_sf(data = animationDatalist()$data, aes(#x = animationDatalist()$data$X.1, y = animationDatalist()$data$Y.1,
                                                                   size = 10,
-                                                                  color = animationDatalist()$data$movement_only, group = animationDatalist()$data$weeks_since))+
+                                                                  color = animationDatalist()$data$movement_only, group = animationDatalist()$data$TAG))+
                   transition_time(weeks_since) +
                   ggtitle(
-                    #paste("Date", m3$Date),
+                    
                     paste(input$anim_Title, '{frame_time}'),
-                    subtitle = paste("Week {frame} of {nframes} past Initial Date of", min(animationDatalist()$data$Date) ))
+                    subtitle = paste("Week {frame} of {nframes} past Initial Date of", min(animationDatalist()$data$Date)))
+                
                 map_with_data
+                
                 anim_save("WindyGapFishMovements.gif", animate(map_with_data, nframes = animationDatalist()$num_weeks, fps = input$fps_Slider, height = 1200, width =1200)) # New
                 
                 
               } else if (input$radio2 == "days"){
                 map_with_data <- map_with_data + 
-                  geom_point(data = animationDatalist()$data, aes(x = animationDatalist()$data$X.1, y = animationDatalist()$data$Y.1,
+                  geom_sf(data = animationDatalist()$data, aes(#x = animationDatalist()$data$X.1, y = animationDatalist()$data$Y.1,
                                                                   size = 10,
-                                                                  color = animationDatalist()$data$movement_only, group = animationDatalist()$data$days_since))+
+                                                                  color = animationDatalist()$data$movement_only, group = animationDatalist()$data$TAG))+
                   transition_time(days_since) + 
                   labs(title = "Days") +
                   ggtitle(
                     
                     paste(input$anim_Title, '{frame_time}'),
-                    subtitle = paste("Day {frame} of {nframes} past Initial Date of", min(animationDatalist()$data$Date) ))
+                    subtitle = paste("Day {frame} of {nframes} past Initial Date of", min(animationDatalist()$data$Date)))
+                
                 map_with_data
-                anim_save("WindyGapFishMovements.gif", animate(map_with_data, nframes = animationDatalist()$num_days,  renderer = gifski_renderer(), fps = input$fps_Slider, height = 1200, width =1200)) # New
+                
+                anim_save("WindyGapFishMovements.gif", animate(map_with_data, nframes = animationDatalist()$num_days, fps = input$fps_Slider, height = 1200, width =1200)) # New
                 
               }
             )#end of isolate
