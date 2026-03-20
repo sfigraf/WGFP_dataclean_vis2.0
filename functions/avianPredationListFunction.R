70734f8efcfa7e840745ebeb1778c24f91c7bb59
Sam Graf <sfigraf@gmail.com>
2026-03-20 17:42:00 -0600

    adeded sculpin distance moved check 500 m total movment

--- functions/avianPredationListFunction.R
+++ functions/avianPredationListFunction.R
@@ -1,5 +1,6 @@
 avianPredationListFunction <- function(All_Events = AllCombinedEvents$df_list$All_Events, 
-                                       AvianPredation = AvianPredation, enc_hist_wide_listPossibleAvianPredation = enc_hist_wide_list$enc_wide_list$possibleAvianPredation, 
+                                       AvianPredation = AvianPredation, 
+                                       enc_hist_wide_listPossibleAvianPredation = enc_hist_wide_list$enc_wide_list$predationList, 
                                        encounterMARKStates = encounterMARKStates, 
                                        Movements_list = Movements_list, ghostdata = GhostTags, 
                                        checkedTags = checkedTags) {
@@ -17,7 +18,8 @@ avianPredationListFunction <- function(All_Events = AllCombinedEvents$df_list$Al
   avianPredationList <- list(
     "movingDownstream" = movingDownstream, 
     "movingUpstream" = movingUpstream, 
-    "largeMovementsWithoutChannel" = enc_hist_wide_listPossibleAvianPredation, 
+    "largeMovementsWithoutChannel" = enc_hist_wide_listPossibleAvianPredation$possibleAvianPredation, 
+    "flaggedSculpin" = enc_hist_wide_listPossibleAvianPredation$sculpinFlaggedMovements, 
     "statesWeeklyActiveFish" = encounterMARKStates$possibleAvianPredation$weeklyActiveFish, 
     "statesAllActiveFish" = encounterMARKStates$possibleAvianPredation$overAllActiveFishNotinWeeklyDF, 
     "fastMovements" = Movements_list$avianPredationDFS$fastMovements,
@@ -42,7 +44,7 @@ avianPredationListFunction <- function(All_Events = AllCombinedEvents$df_list$Al
   
   checkedTags$TAG <- as.character(checkedTags$TAG)
   
-  ##filter out MERG tags
+  ##see MERG tags, turn them purple
   #get tag data first bc no species field in avian predation dfs
   taggedBirdEventsOnly = AllCombinedEvents$df_list$All_Events %>%
     #add more bird species if that arises
